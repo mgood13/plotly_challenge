49c79b436fa9f3638f593c67389e2b11a5fe9818
Michael Good <michael@Michaels-MacBook-Pro-3.local>
2021-01-09 00:33:20 -0500

    Adding in the demographics switch statment

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var titleInit = d3.select("#userID");
 var temptitle = titleInit.text();
-titleInit.text(temptitle + " 940")
+titleInit.text(temptitle + " ---")
 
 d3.json("samples.json").then((sampleData) => {
     var names = sampleData.names;
@@ -8,6 +8,8 @@ d3.json("samples.json").then((sampleData) => {
     var samples = sampleData.samples;
 
     var select = d3.select("#selDataset");
+    var firstElement = select.append("option")
+    firstElement.text('---')
 
     names.forEach((dataID)=>{
         var element = select.append("option");
@@ -21,7 +23,74 @@ d3.json("samples.json").then((sampleData) => {
         var temp = titletag.text().split(' ');
         temp.pop();
         temp = temp.join(" ")
-        titletag.text(temp + " " +  this.value)
+        var userID = this.value
+        if (userID == '---') {
+        return;
+
+        };
+
+
+        titletag.text(temp + " " +  userID)
+
+        mdata.forEach((participant)=>{
+            if (userID == participant.id){
+                var list = d3.select(".demographics");
+                var keys = Object.keys(participant);
+                var values = Object.values(participant);
+
+                for (var i=0; i < keys.length; i++){
+                    console.log(i)
+                    switch(i){
+                        case 1:
+                            var item = list.append('li');
+                            var key = keys[i];
+                            key = key.charAt(0).toUpperCase() + key.slice(1);
+                            item.text(key + ": " + values[i])
+                        break;
+
+                        case 2:
+                            var item = list.append('li');
+                            var key = keys[i];
+                            key = key.charAt(0).toUpperCase() + key.slice(1);
+                            item.text(key + ": " + values[i])
+                        break;
+
+                        case 3:
+                            var item = list.append('li');
+                            var key = keys[i];
+                            key = key.charAt(0).toUpperCase() + key.slice(1);
+                            item.text(key + ": " + values[i])
+                        break;
+
+                        case 4:
+                            var item = list.append('li');
+                            var key = keys[i];
+                            key = key.charAt(0).toUpperCase() + key.slice(1);
+                            item.text(key + ": " + values[i])
+                        break;
+
+                        case 5:
+                            var item = list.append('li');
+                            item.text("Belly Button Type: " + values[i])
+                        break;
+
+                        case 6:
+                            var item = list.append('li');
+                            item.text("Washing Frequency (Scrubs per week): " + values[i])
+
+                        break;
+
+                        default:
+                            console.log('Skipping over ID')
+                            d3.select(".demographics").selectAll('li').remove()
+
+
+
+                    }
+                };
+            };
+        });
+
 });
 
 
