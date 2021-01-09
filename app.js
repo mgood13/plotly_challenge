fed400ba4b00441b473f28ec40c1e603efce5315
Michael Good <michael@Michaels-MacBook-Pro-3.local>
2021-01-08 23:45:44 -0500

    Adding the id to the title string:wq

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+var titleInit = d3.select("#userID");
+var temptitle = titleInit.text();
+titleInit.text(temptitle + " 940")
+
 d3.json("samples.json").then((sampleData) => {
     var names = sampleData.names;
     var mdata = sampleData.metadata;
@@ -7,10 +11,18 @@ d3.json("samples.json").then((sampleData) => {
 
     names.forEach((dataID)=>{
         var element = select.append("option");
-        element.text(dataID)
+        element.text(dataID);
     });
 
 
+
+    select.on('change', function() {
+        var titletag = d3.select("#userID");
+        var temp = titletag.text().split(' ');
+        temp.pop();
+        temp = temp.join(" ")
+        titletag.text(temp + " " +  this.value)
+});
 
 
 
